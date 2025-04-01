7aedc9773a942b56c06e6685e23df010f0959021
YutaTRM <murakumo0079@gmail.com>
2025-04-01 22:53:33 +0900

    とりあえずのmetadata追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,46 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: '座席予想アプリ - Seat Predicter',
-  description: 'ライブコンサートの座席情報を共有するアプリケーション',
-}
+  title: {
+    default: "ライブ座席予想(β版)",
+    template: "%s | ライブ座席予想(β版)"
+  },
+  description: "ライブ座席予想(β版)は、チケット番号を登録/一覧化しブロックの情報を可視化するアプリです。",
+  keywords: ["座席予想", "ライブ", "コンサート", "オタ活", "チケット情報", "チケット一覧","座席一覧","ブロック予想"],
+  appleWebApp: true,
+  openGraph: {
+    title: "ライブ座席予想(β版)",
+    description: "映ライブ座席予想(β版)は、チケット番号を登録/一覧化しブロックの情報を可視化するアプリ",
+    type: "website",
+    url: "https://cast-finder.vercel.app",
+    siteName: "ライブ座席予想(β版)",
+    // images: [
+    //   {
+    //     url: "/images/og-image.png", // OG画像のパスを適切に設定
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // ],
+  },
+  twitter: {
+    // card: "summary_large_image",
+    title: "ライブ座席予想(β版)",
+    description: "チケット番号を登録/一覧化し、ブロックの情報を可視化するアプリ",
+    // images: ["/images/twitter-image.png"], // Twitter Card画像のパスを適切に設定
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
