082eedbada08180d1e68970fc004fa10634e5978
YutaTRM <murakumo0079@gmail.com>
2025-04-11 10:38:12 +0900

    ホーム画面追加用の設定ファイルを追加

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: '座席予想掲示板(β)',
+    short_name: '座席予想',
+    description: '座席予想掲示板(β)は、チケット番号を登録/一覧化しブロックの情報を可視化するアプリです。',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff1f2',
+    theme_color: '#fff1f2',
+    icons: [
+      {
+        src: '/images/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: 'images/icon.png',
+        sizes: '320x320',
+        type: 'image/png',
+      },
+    ],
+  }
+}
