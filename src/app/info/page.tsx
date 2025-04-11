00e7a805b513aadfb13647ce1a3514430bbe1903
YutaTRM <murakumo0079@gmail.com>
2025-04-11 11:45:21 +0900

    ヘッダーコンポーネントを適用

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SectionHeader from '../components/common/SectionHeader';
 import InfoList from '../components/home/InfoList';
 
 export default function InfoPage() {
   return (
     <main className="container mx-auto h-screen overflow-y-auto min-h-screen px-4 py-8 text-md">
-      <h1 className="text-2xl font-bold text-rose-500 mb-6 text-center">更新情報</h1>
+      <SectionHeader title="更新情報" />
       <InfoList/>
     </main>
   );
