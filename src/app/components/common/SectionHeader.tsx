ba9bba8708118df4f2935a2ee2ff6b5aff2bae4a
YutaTRM <murakumo0079@gmail.com>
2025-04-11 17:02:57 +0900

    戻るアイコンをコンポーネントに置き換え

--- src/app/components/common/SectionHeader.tsx
+++ src/app/components/common/SectionHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Icon from './Icon';
 
 interface SectionHeaderProps {
   title: string;
@@ -19,7 +20,7 @@ export default function SectionHeader({ title }: SectionHeaderProps) {
         className="text-gray-600 hover:text-gray-800 transition-colors"
         aria-label="トップページに戻る"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M5 12l6 6" /><path d="M5 12l6 -6" /></svg>
+        <Icon type='prev'/>
       </Link>
       <h1 className="font-bold text-rose-500">{title}</h1>
       <span>　</span>
